9dd7ddf91693740d4d757c6234908a911b05902c
Davi Camarinha <davicamarinha@gmail.com>
2019-04-04 23:50:04 -0300

    primeiro commit

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div className="container">
+            <h1>Titulo</h1>  
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
