ab75c3240614047383184c3890e216d0c8583e4b
Davi Camarinha <davi.camarinha@fulllab.com.br>
2019-04-05 18:59:17 -0300

    Estudos

--- src/components/header.js
+++ src/components/header.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 
 
 class Header extends Component {
-  render() {
-    return (
-      <header>
-        <div className="container">
-            <h1>Titulo</h1>  
-        </div>
-      </header>
-    );
-  }
+	render() {
+		return (
+			<header className="default-header">
+				<div className="container">
+					<div className="row">
+						<div className="col-sm-3">Teste 1</div>
+						<div className="col-sm-9">Teste 2</div>
+						
+					</div>
+				</div>
+			</header>
+		);
+	}
 }
 
 export default Header;
